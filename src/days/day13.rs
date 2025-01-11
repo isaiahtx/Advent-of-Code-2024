f5cbaee12fb1c01f8122e0f944c322ff6581dd56
Isaiah Dailey <isaiahdaileytx@gmail.com>
2025-01-10 22:05:41 -0600

    day 14 & day 13 update

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -3,33 +3,31 @@ use crate::utils::LinesIterator;
 
 #[derive(Debug)]
 struct Machine {
-    a: (usize, usize),
-    b: (usize, usize),
-    prize: (usize, usize),
+    a: (u64, u64),
+    b: (u64, u64),
+    prize: (u64, u64),
 }
 
 const A_COST: usize = 3;
 const B_COST: usize = 1;
 
 impl Machine {
-    const fn new(a: (usize, usize), b: (usize, usize), prize: (usize, usize)) -> Self {
+    const fn new(a: (u64, u64), b: (u64, u64), prize: (u64, u64)) -> Self {
         Self { a, b, prize }
     }
 
-    fn cost(&self) -> Option<usize> {
-        let get_edges = |coords: (usize, usize)| {
+    fn cost(&self) -> Option<u64> {
+        let get_edges = |coords: (u64, u64)| {
             let mut output = Vec::new();
 
             let ax = coords.0 + self.a.0;
             let ay = coords.1 + self.a.1;
-
             if ax <= self.prize.0 && ay <= self.prize.1 {
                 output.push((A_COST, (ax, ay)));
             }
 
             let bx = coords.0 + self.b.0;
             let by = coords.1 + self.b.1;
-
             if bx <= self.prize.0 && by <= self.prize.1 {
                 output.push((B_COST, (bx, by)));
             }
@@ -37,7 +35,83 @@ impl Machine {
             output
         };
 
-        shortest_path_cost((0, 0), self.prize, get_edges)
+        shortest_path_cost((0, 0), self.prize, get_edges).map(|x| x as u64)
+    }
+
+    /// Write A = [a.0, b.0 ; a.1, b.1] and p = (prize.0, prize.1). Then a
+    /// valid move to the prize is the data of any 2d vector n = (n.0, n.1)
+    /// such that An = s.
+    #[allow(clippy::redundant_else)]
+    const fn cost2(&self) -> Option<u64> {
+        let a1 = self.a.0;
+        let a2 = self.a.1;
+
+        let b1 = self.b.0;
+        let b2 = self.b.1;
+
+        let p1 = self.prize.0;
+        let p2 = self.prize.1;
+
+        let mut output = None;
+
+        if a1 * b2 == a2 * b1 {
+            // Originally I wrote the following (incorrect) algorithm, but then
+            // I realized the input Advent of Code gave me does not contain the
+            // singular case, so this can just be ignored.
+
+            // let n = p1 / a1;
+            // let m = p1 / b1;
+
+            // // Greedy algorithm, not sure if it works
+
+            // // To start, we get as close as we can to the target (p1) from
+            // // below, as cheaply as possible.
+            // if (n * A_COST as u64) < (m * B_COST as u64) {
+            //     // In this case, it is cheaper to get right below using a
+            //     // instead of b.
+
+            //     // The remainder that we need to get using b, if it's possible.
+            //     // So we need to figure out what m should be.
+
+            //     // remainder = p1 - n * a1
+            //     let remainder = p1.checked_sub(n * a1).unwrap();
+
+            //     if (remainder % b1) == 0 {
+            //         // if we can get the remainder using b...
+
+            //         // then set m!
+            //         let m = remainder / b1;
+
+            //         // We found n and m for the first coordinate, make sure they
+            //         // also work for the second coordinate.
+            //         if n * a2 + m * b2 == p2 {
+            //             output = Some(n * A_COST as u64 + m * B_COST as u64);
+            //         }
+            //     }
+            // } else {
+            //     // remainder = p1 - m * b1
+            //     let remainder = p1.checked_sub(m * b1).unwrap();
+
+            //     if (remainder % a1) == 0 {
+            //         let m = remainder / a1;
+            //         if n * a2 + m * b2 == p2 {
+            //             output = Some(n * A_COST as u64 + m * B_COST as u64);
+            //         }
+            //     }
+            // }
+        } else {
+            let det = (a1 * b2) as i128 - (a2 * b1) as i128;
+            let m1 = (b2 * p1) as i128 - (b1 * p2) as i128;
+            let m2 = (a1 * p2) as i128 - (a2 * p1) as i128;
+            if m1 % det == 0 && m2 % det == 0 {
+                let n1 = m1 / det;
+                let n2 = m2 / det;
+                if n1 >= 0 && n2 >= 0 {
+                    output = Some((n1 as u64 * A_COST as u64) + (n2 as u64 * B_COST as u64));
+                }
+            }
+        }
+        output
     }
 }
 
@@ -46,17 +120,17 @@ fn parse_input(lines: &mut LinesIterator) -> Vec<Machine> {
 
     loop {
         let a_line = lines.next().unwrap().unwrap();
-        let a1 = a_line[12..14].parse::<usize>().unwrap();
-        let a2 = a_line[18..20].parse::<usize>().unwrap();
+        let a1 = a_line[12..14].parse::<u64>().unwrap();
+        let a2 = a_line[18..20].parse::<u64>().unwrap();
 
         let b_line = lines.next().unwrap().unwrap();
-        let b1 = b_line[12..14].parse::<usize>().unwrap();
-        let b2 = b_line[18..20].parse::<usize>().unwrap();
+        let b1 = b_line[12..14].parse::<u64>().unwrap();
+        let b2 = b_line[18..20].parse::<u64>().unwrap();
 
         let p_line = &lines.next().unwrap().unwrap()[9..];
         let mut p_line = p_line.split(", ");
-        let p1 = p_line.next().unwrap().parse::<usize>().unwrap();
-        let p2 = p_line.next().unwrap()[2..].parse::<usize>().unwrap();
+        let p1 = p_line.next().unwrap().parse::<u64>().unwrap();
+        let p2 = p_line.next().unwrap()[2..].parse::<u64>().unwrap();
 
         output.push(Machine::new((a1, a2), (b1, b2), (p1, p2)));
 
@@ -68,13 +142,45 @@ fn parse_input(lines: &mut LinesIterator) -> Vec<Machine> {
     output
 }
 
+fn parse_input_2(lines: &mut LinesIterator) -> Vec<Machine> {
+    let mut output = Vec::new();
+
+    loop {
+        let a_line = lines.next().unwrap().unwrap();
+        let a1 = a_line[12..14].parse::<u64>().unwrap();
+        let a2 = a_line[18..20].parse::<u64>().unwrap();
+
+        let b_line = lines.next().unwrap().unwrap();
+        let b1 = b_line[12..14].parse::<u64>().unwrap();
+        let b2 = b_line[18..20].parse::<u64>().unwrap();
+
+        let p_line = &lines.next().unwrap().unwrap()[9..];
+        let mut p_line = p_line.split(", ");
+        let p1 = p_line.next().unwrap().parse::<u64>().unwrap();
+        let p2 = p_line.next().unwrap()[2..].parse::<u64>().unwrap();
+
+        output.push(Machine::new(
+            (a1, a2),
+            (b1, b2),
+            (p1 + 10000000000000, p2 + 10000000000000),
+        ));
+
+        if lines.next().is_none() {
+            break;
+        }
+    }
+
+    output
+}
+
 pub fn run1(lines: &mut LinesIterator) -> String {
-    let output: usize = parse_input(lines).iter().filter_map(Machine::cost).sum();
+    let output: u64 = parse_input(lines).iter().filter_map(Machine::cost).sum();
 
     format!("{output}")
 }
 
 pub fn run2(lines: &mut LinesIterator) -> String {
-    lines.next();
-    format!("{lines:?}")
+    let output: u64 = parse_input_2(lines).iter().filter_map(Machine::cost2).sum();
+
+    format!("{output}")
 }
--- src/days/day14.rs
+++ src/days/day14.rs
@@ -1,11 +1,128 @@
 use crate::utils::LinesIterator;
+use std::cmp::Ordering::{Equal, Greater, Less};
+use std::io;
+use std::io::Write;
+use std::marker::PhantomData;
+
+type Coords = (i32, i32);
+type Velocity = (i32, i32);
+
+const HEIGHT: i32 = 103;
+const WIDTH: i32 = 101;
+
+fn parse_input(lines: &mut LinesIterator) -> Vec<(Coords, Velocity)> {
+    let mut output = Vec::new();
+    for line in lines {
+        let line = line.unwrap();
+        let mut line = line.split(' ');
+
+        let mut pos = line.next().unwrap()[2..].split(',');
+        let p2 = pos.next().unwrap().parse::<i32>().unwrap();
+        let p1 = pos.next().unwrap().parse::<i32>().unwrap();
+
+        let mut vel = line.next().unwrap()[2..].split(',');
+        let v2 = vel.next().unwrap().parse::<i32>().unwrap();
+        let v1 = vel.next().unwrap().parse::<i32>().unwrap();
+
+        output.push(((p1, p2), (v1, v2)));
+    }
+
+    output
+}
+
+const fn update(robot: (Coords, Velocity), seconds: i32) -> (Coords, Velocity) {
+    let p1 = robot.0 .0;
+    let p2 = robot.0 .1;
+
+    let v1 = robot.1 .0;
+    let v2 = robot.1 .1;
+
+    let new_p1 = (p1 + v1 * seconds).rem_euclid(HEIGHT);
+    let new_p2 = (p2 + v2 * seconds).rem_euclid(WIDTH);
+
+    ((new_p1, new_p2), (v1, v2))
+}
 
 pub fn run1(lines: &mut LinesIterator) -> String {
-    lines.next();
-    format!("{lines:?}")
+    let robots = parse_input(lines);
+
+    let mut top_left = 0;
+    let mut top_right = 0;
+    let mut bottom_left = 0;
+    let mut bottom_right = 0;
+
+    for robot in robots {
+        let height_middle = (HEIGHT - 1) / 2;
+        let width_middle = (WIDTH - 1) / 2;
+
+        let (p1, p2) = update(robot, 100).0;
+
+        match (p1.cmp(&height_middle), p2.cmp(&width_middle)) {
+            (Less, Less) => top_left += 1,
+            (Less, Greater) => top_right += 1,
+            (Greater, Less) => bottom_left += 1,
+            (Greater, Greater) => bottom_right += 1,
+            _ => {}
+        }
+    }
+
+    let output = top_left * top_right * bottom_left * bottom_right;
+
+    format!("{output}")
+}
+
+fn chars_grid_to_string(chars: &Vec<Vec<char>>) -> String {
+    chars
+        .into_iter()
+        .map(|l| {
+            let mut s = l.into_iter().collect::<String>();
+            s.push('\n');
+            s
+        })
+        .collect()
+}
+
+fn robots_to_grid(robots: &Vec<(Coords, Velocity)>) -> Vec<Vec<char>> {
+    let mut chars = Vec::with_capacity(HEIGHT as usize);
+
+    for _ in 0..HEIGHT {
+        chars.push(Vec::from([' '; WIDTH as usize]));
+    }
+
+    for ((r, c), _) in robots {
+        chars[*r as usize][*c as usize] = '█';
+    }
+
+    chars
 }
 
 pub fn run2(lines: &mut LinesIterator) -> String {
-    lines.next();
-    format!("{lines:?}")
+    let mut robots = parse_input(lines);
+    let mut _b = String::new();
+    let mut n = 0;
+
+    loop {
+        let chars = robots_to_grid(&robots);
+
+        for (i, row) in chars.iter().enumerate() {
+            for (j, c) in row.iter().enumerate() {
+                if i + 4 < HEIGHT as usize
+                    && *c == '█'
+                    && chars[i + 1][j] == '█'
+                    && chars[i + 2][j] == '█'
+                    && chars[i + 3][j] == '█'
+                    && chars[i + 4][j] == '█'
+                {
+                    println!("\r{}\n\n", chars_grid_to_string(&chars));
+                    println!("{n}");
+                    io::stdout().flush().unwrap();
+
+                    let _ = io::stdin().read_line(&mut _b);
+                }
+            }
+        }
+
+        robots = robots.into_iter().map(|r| update(r, 1)).collect();
+        n += 1;
+    }
 }
